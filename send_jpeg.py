421cc7949b974451be90961738be54dd8f33bb63
Francois Fernando <francois@fernando.onl>
2019-05-03 22:17:08 +0100

    specify hub host

--- send_jpeg.py
+++ send_jpeg.py
@@ -1,12 +1,22 @@
 
+
 # run this program on each RPi to send a labelled image stream
 import socket
 import time
 from picamutil import PiJpegStream
 import imagezmq.imagezmq as imagezmq
 import cv2
+import argparse
 
-sender = imagezmq.ImageSender(connect_to='tcp://192.168.10.183:5555')
+ap = argparse.ArgumentParser()
+ap.add_argument("-H", "--hub", required=True,
+    help="image hub ip address or host name")
+args = vars(ap.parse_args())
+
+hub_host = args["hub"]
+hub_host_url = 'tcp://{}:5555'.format(hub_host)
+print('hub host: {}'.format(hub_host_url))
+sender = imagezmq.ImageSender(connect_to=hub_host_url)
 
 rpi_name = socket.gethostname() # send RPi hostname with each image
 picam = PiJpegStream(resolution=(640, 480), framerate=16).start()
@@ -24,4 +34,6 @@ while True:  # send images as stream until Ctrl-C
     avg_time_to_send = time_to_send if avg_time_to_send is None else (avg_time_to_send + time_to_send) / 2
     print('sent image in {} s'.format(avg_time_to_send))
     time.sleep(0.05)
+
+picam.close()
 
